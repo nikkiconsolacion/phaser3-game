8b2711352ff112e67bacae1ff11052f6985189a7
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-11-19 17:57:13 -0800

    platforms

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ const config = {
   parent: "phaser-example",
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 300 },
+        debug: false
+    }
+  },
   scene: {
     preload: preload,
     create: create
@@ -14,19 +21,42 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-function preload() {
-  this.load.image("logo", logoImg);
+// function preload() {
+//   this.load.image("logo", logoImg);
+// }
+
+// function create() {
+//   const logo = this.add.image(400, 150, "logo");
+
+//   this.tweens.add({
+//     targets: logo,
+//     y: 450,
+//     duration: 2000,
+//     ease: "Power2",
+//     yoyo: true,
+//     loop: -1
+//   });
+// }
+
+function preload () {
+  this.load.image('sky', 'src/assets/sky.png');
+  this.load.image('ground', 'src/assets/platform.png');
+  this.load.image('star', 'src/assets/star.png');
+  this.load.image('bomb', 'src/assets/bomb.png');
+  this.load.spritesheet('dude', 'src/assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
-function create() {
-  const logo = this.add.image(400, 150, "logo");
+var platforms;
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
+function create () {
+  //order matters
+  this.add.image(400, 300, 'sky');
+
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+  platforms.create(600, 400, 'ground');
+  platforms.create(50, 250, 'ground');
+  platforms.create(750, 220, 'ground');
 }
