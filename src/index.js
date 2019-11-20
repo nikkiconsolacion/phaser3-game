e7f4aa0e2470ad422dc96cfdfdf5c198bc793201
Nikki Consolacion <nikkiconsolacion@gmail.com>
2019-11-19 20:14:13 -0800

    added bombs and gameOver

--- src/index.js
+++ src/index.js
@@ -50,9 +50,11 @@ function preload() {
 //variables
 let player;
 let stars;
+let bombs;
 let platforms;
 let cursors;
 let score = 0;
+let gameOver = false;
 let scoreText;
 
 function create() {
@@ -106,18 +108,29 @@ function create() {
     child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
   });
 
+  //bombs
+  bombs = this.physics.add.group();
+
   //score diplayed
   scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
   //Collisions
   this.physics.add.collider(player, platforms);
   this.physics.add.collider(stars, platforms);
+  this.physics.add.collider(bombs, platforms);
 
   //Overlap
   this.physics.add.overlap(player, stars, collectStar, null, this); //when player runs over stars, collect them
+
+  this.physics.add.collider(player, bombs, hitBomb, null, this);
+
 }
 
 function update() {
+  if (gameOver) {
+    return;
+  }
+
   //player movement
   if (cursors.left.isDown) {
     player.setVelocityX(-160);
@@ -131,14 +144,14 @@ function update() {
     player.setVelocityX(0);
     player.anims.play('turn');
   }
-  if (cursors.up.isDown && player.body.touching.down) {
-    //player can only jump off the ground
-    player.setVelocityY(-430);
-  }
-  // if (cursors.up.isDown) {
-  //   //player can jump mid air
-  //   player.setVelocityY(-200);
+  // if (cursors.up.isDown && player.body.touching.down) {
+  //   //player can only jump off the ground
+  //   player.setVelocityY(-430);
   // }
+  if (cursors.up.isDown) {
+    //player can jump mid air
+    player.setVelocityY(-200);
+  }
 }
 
 function collectStar (player, star) {
@@ -146,4 +159,26 @@ function collectStar (player, star) {
   star.disableBody(true, true);
   score += 10;
   scoreText.setText('Score: ' + score);
+
+  if (stars.countActive(true) === 0) {
+    //  A new batch of stars to collect
+    stars.children.iterate(function (child) {
+      child.enableBody(true, child.x, 0, true, true);
+    });
+
+    let x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+    let bomb = bombs.create(x, 16, 'bomb');
+    bomb.setBounce(1);
+    bomb.setCollideWorldBounds(true);
+    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    bomb.allowGravity = false;
+  }
+}
+
+function hitBomb (player, bomb) {
+  this.physics.pause();
+  player.setTint(0xff0000); //turns the player red
+  player.anims.play('turn');
+  gameOver = true;
 }
